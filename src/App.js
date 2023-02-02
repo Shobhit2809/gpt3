0c26adb23f720cf12ddf25d6459a34531d901542
Shobhit Agrawal <shobhitagrawal2809@gmail.com>
2023-02-02 20:23:29 +0530

    file and folder structure added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import './App.css'
+
+import {Footer,Blog,Possibility,Features,whatGPT3,Header} from './containers'
+import {CTA,Brand,Navbar} from './Components'
 
 const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className="app">
+        <div className="gradient_bg">
+            <Navbar/>
+            <Header/>
+        </div>
+        <Brand/>
+        <whatGPT3/>
+        <Features/>
+        <Possibility/>
+        <CTA/>
+        <Blog/>
+        <Footer/>
     </div>
-  )
+    )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import  ReactDOM  from 'react-dom'
 import App from './App'
-
+import './index.css'
 // this app component is going to be rendered inside a div with an id of root 
 // see index.html inside public
 ReactDOM.render(<App/>,document.getElementById('root'))
